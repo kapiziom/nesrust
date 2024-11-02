a030cc237b34bf9b3f529b0326d443bf270018e1
KTomczak <ktomczak1996@gmail.com>
2024-11-02 15:43:40 +0100

    -

--- src/emulator/core/memory_bus.rs
+++ src/emulator/core/memory_bus.rs
@@ -51,6 +51,24 @@ mod tests {
     }
 
     #[test]
+    fn test_memory_write_read_u16() {
+        let mut bus = MemoryBus::new();
+
+        bus.write(0x2000, 0x42);
+        bus.write(0x2001, 0x42);
+        assert_eq!(bus.memory[0x2000], 0x42);
+        assert_eq!(bus.memory[0x2001], 0x42);
+
+        let value0 = bus.read(0x2000);
+        assert_eq!(value0, 0x42);
+        let value1 = bus.read(0x2001);
+        assert_eq!(value1, 0x42);
+
+        let value2 = bus.read_u16(0x2000);
+        assert_eq!(value2, 0x4242);
+    }
+
+    #[test]
     fn test_memory_read_write_u16() {
         let mut bus = MemoryBus::new();
         bus.write_u16(0x3000, 0x1234);
--- src/emulator/core/mock_bus.rs
+++ src/emulator/core/mock_bus.rs
@@ -39,18 +39,38 @@ mod tests {
     #[test]
     fn test_memory_read_write() {
         let mut bus = MockBus::new();
-        bus.write(0x2000, 0x42);
-        let value = bus.read(0x2000);
 
+        bus.write(0x2000, 0x42);
+        assert_eq!(bus.memory[0x2000], 0x42);
+
+        let value = bus.read(0x2000);
         assert_eq!(value, 0x42);
+    }
+
+    #[test]
+    fn test_memory_write_read_u16() {
+        let mut bus = MockBus::new();
+
+        bus.write(0x2000, 0x42);
+        bus.write(0x2001, 0x42);
+        assert_eq!(bus.memory[0x2000], 0x42);
+        assert_eq!(bus.memory[0x2001], 0x42);
+
+        let value0 = bus.read(0x2000);
+        assert_eq!(value0, 0x42);
+        let value1 = bus.read(0x2001);
+        assert_eq!(value1, 0x42);
+
+        let value2 = bus.read_u16(0x2000);
+        assert_eq!(value2, 0x4242);
     }
 
     #[test]
     fn test_memory_read_write_u16() {
         let mut bus = MockBus::new();
+
         bus.write_u16(0x3000, 0x1234);
         let value = bus.read_u16(0x3000);
-
         assert_eq!(value, 0x1234);
     }
 
